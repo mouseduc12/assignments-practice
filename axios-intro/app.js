32640020a8c9fca9533c1fc9978322b96a3802b5
Duc Pham <bloomsybusiness@yahoo.com>
2018-11-02 16:23:01 -0600

    New Commit

---
+++ axios-intro/app.js
@@ -0,0 +1,40 @@
+//Get one specific item
+
+// axios.get('https://swapi.co/api/people/' + id).then(function(response){
+//     console.log(response.data)
+// }).catch(function(error){
+//     console.log(error)
+// })
+
+
+
+//Get all the items in an object
+
+// axios.get('https://api.vschool.io/marcus/todo/').then(function(response){
+//     console.log(response.data)
+// }).catch(function(error){
+//     console.log(error)
+// })
+
+
+
+//Post a new item
+// var newTodo = {
+//     title: 'intro to axios',
+//     description: "isn't it nice?",
+//     price: 0,
+//     completed: false
+// }
+
+// axios.post('https://api.vschool.io/marcus/todo/', newTodo).then(function(response){
+//     console.log('Successfully posted')
+// }).catch(function(error){
+//     console.log(error)
+// })
+
+
+
+//Edit or update an existing item
+// var updatedTodo = {
+//     completed: true
+// }
